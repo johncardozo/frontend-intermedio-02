c0efdd539df3f3e33ac1ac6c60d982dfd5fd4651
John Cardozo <johncardozo@gmail.com>
2023-09-14 06:16:10 -0500

    JS: Operadores aritméticos

---
+++ javascript/js/aritmetica.js
@@ -0,0 +1,47 @@
+// SUMA
+let suma1 = 10 + 5;
+console.log(suma1);
+
+let suma2 = suma1 + 20;
+console.log(suma2);
+
+let suma3 = suma1 + suma2;
+console.log(suma3);
+
+// Shortcut de incremento/decremento
+suma3++;
+console.log(suma3);
+
+// Acumulación
+suma3 = suma3 + 10;
+console.log(suma3);
+suma3 += 10;
+console.log(suma3);
+
+// RESTA
+let resta1 = 10 - 5;
+console.log(resta1);
+
+resta1--;
+console.log(resta1);
+
+resta1 -= 5;
+console.log(resta1);
+
+// MULTIPLICACIÓN
+let mult1 = 2 * 3;
+console.log(mult1);
+
+mult1 *= 3;
+console.log(mult1);
+
+// DIVISIÓN
+let div1 = 20 / 2;
+console.log(div1);
+
+div1 /= 2;
+console.log(div1);
+
+// MÓDULO
+let residuo1 = 20 % 6;
+console.log(residuo1);
