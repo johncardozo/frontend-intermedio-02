194992ef2f2a9ebd4de3089d608bef0dba9c1459
John Cardozo <johncardozo@gmail.com>
2023-09-22 07:53:08 -0500

    JS: Objetos JSON - Propiedades

--- javascript/js/objetos-json.js
+++ javascript/js/objetos-json.js
@@ -3,7 +3,6 @@
 
 let variable = 4;
 let arreglos = [1, 2, 3, 4];
-
 let objeto = {
   nombre: "Catalina",
   email: "cata@gmail.com",
@@ -26,3 +25,46 @@ console.log(objeto.hobbies[1]);
 objeto.hobbies.forEach((hobby) => console.log(hobby));
 // Ejecuta una función de un objeto -> método
 console.log(objeto.esAdulto());
+
+let banda = "The Beatles";
+// Definición de un objeto
+let usuario = {
+  nombre: "John",
+  email: "john@gmail.com",
+  banda, // abreviación de la asignación de una variable a una propiedad
+};
+
+console.log(usuario);
+
+// Modifica un propiedad de un objeto
+usuario.nombre = "Paul";
+console.log(usuario);
+
+// Si la propiedad no existe, la crea
+usuario.ciudad = "Liverpool";
+usuario.email = "paul@gmai.com";
+console.log(usuario);
+
+// Elimina una propiedad del objeto
+delete usuario.ciudad;
+console.log(usuario);
+
+// Verifica si una propiedad existe en un objeto
+let tieneCiudad = usuario.hasOwnProperty("ciudad");
+console.log(tieneCiudad);
+let tieneNombre = usuario.hasOwnProperty("nombre");
+console.log(tieneNombre);
+
+const ubicacion = {
+  ciudad: "Medellin",
+  departamento: "Antioquia",
+  pais: "Colombia",
+};
+
+console.log(ubicacion);
+
+// Bloquea la modificación del objeto
+Object.freeze(ubicacion);
+ubicacion.ciudad = "Envigado";
+
+console.log(ubicacion);
