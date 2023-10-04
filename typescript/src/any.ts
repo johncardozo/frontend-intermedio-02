6d178130137fc29f52212cd3123e48355d99bffb
John Cardozo <johncardozo@gmail.com>
2023-10-04 06:12:16 -0500

    TS: Tipo de dato any

---
+++ typescript/src/any.ts
@@ -0,0 +1,16 @@
+// any: Javascript style
+
+// Declaración de variable
+let age: any;
+
+// any: permite asignar cualquier valor
+age = 25;
+age = "Hello";
+age = true;
+
+// Arreglo any
+let arregloCualquiera: any[] = [];
+
+arregloCualquiera.push(25);
+arregloCualquiera.push("Hello");
+arregloCualquiera.push(false);
