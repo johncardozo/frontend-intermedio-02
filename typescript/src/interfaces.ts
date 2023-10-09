bb0c5d18a79c57d894f05f18c289052303978108
John Cardozo <johncardozo@gmail.com>
2023-10-09 07:40:37 -0500

    TD: DOM Tree

--- typescript/src/interfaces.ts
+++ typescript/src/interfaces.ts
@@ -36,6 +36,7 @@ class RunningTraining implements ITraining {
 const entrenamiento1: ITraining = new LongRunTraining(42);
 const entrenamiento2: ITraining = new IntervalsTraining(8, 5);
 const entrenamiento3: ITraining = new RunningTraining(15, "2023-10-09");
+const entrenamiento4: ITraining = new LongRunTraining(55);
 console.log(entrenamiento1);
 console.log(entrenamiento2);
 console.log(entrenamiento3);
@@ -45,7 +46,26 @@ const entrenamientos: ITraining[] = [
   entrenamiento1,
   entrenamiento2,
   entrenamiento3,
+  entrenamiento4,
 ];
 entrenamientos.forEach((e) => {
   console.log(e.format());
+});
+
+// Obtiene una referencia al elemento
+const tbody = document.querySelector("#tbody")!;
+// Recorre la lista de entrenamientos para generar la tabla HTML
+entrenamientos.forEach((entrenamiento) => {
+  // Crea la fila
+  const tr = document.createElement("tr");
+  // Crea las celdas
+  const tdType = document.createElement("td");
+  const tdDistance = document.createElement("td");
+  // Asigna los textos a las celdas dada la información del objeto
+  tdType.textContent = entrenamiento.type;
+  tdDistance.textContent = `${entrenamiento.distance} kms`;
+  // Agrega las celdas a la fila
+  tr.append(tdType, tdDistance);
+  // Agrega la fila a la tabla
+  tbody.append(tr);
 });
