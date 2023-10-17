df65944f1476492f7c6579e51959f7aaefac8039
John Cardozo <johncardozo@gmail.com>
2023-10-17 07:38:43 -0500

    React: Importar CSS en componentes

--- react/task-manager/src/main.jsx
+++ react/task-manager/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+// import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
