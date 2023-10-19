64632e9225d122273786d0b67e27361553b41bb4
John Cardozo <johncardozo@gmail.com>
2023-10-19 06:50:59 -0500

    React: CSS Global

--- react/task-manager/src/App.jsx
+++ react/task-manager/src/App.jsx
@@ -4,10 +4,10 @@ import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <TaskList />
-    </>
+    </div>
   );
 }
 
--- react/task-manager/src/main.jsx
+++ react/task-manager/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./main.css";
 
 const root = document.getElementById("root");
 
