9479a132f39d53ba7280c55fa2cd3b8a9dd62fad
John Cardozo <johncardozo@gmail.com>
2023-10-20 07:54:34 -0500

    React: Iconos con react-icons

--- react/task-manager/src/components/Logo.jsx
+++ react/task-manager/src/components/Logo.jsx
@@ -1,7 +1,12 @@
+import { FaRegCheckCircle } from "react-icons/fa";
 import "../styles/Logo.scss";
 
 function Logo() {
-  return <div className="logo">LOGO</div>;
+  return (
+    <div className="logo">
+      <FaRegCheckCircle />
+    </div>
+  );
 }
 
 export default Logo;
