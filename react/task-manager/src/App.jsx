383d63f6e340390ca2089c696972d4a0e37dfa7d
John Cardozo <johncardozo@gmail.com>
2023-10-30 06:20:07 -0500

    React: Se guardan los datos de lenguajes en un archivo separado

--- react/task-manager/src/App.jsx
+++ react/task-manager/src/App.jsx
@@ -7,35 +7,9 @@ import TaskList from "./components/TaskList";
 import SelectLanguage from "./components/SelectLanguage";
 
 import LocalizationContext from "./context/LocalizationContext";
+import local from "./context/LocalizationData";
 
 import "./styles/App.scss";
-
-const local = {
-  es: {
-    title: "Administrador de Tareas",
-    count: "Cantidad",
-    newTask: "Nueva tarea",
-    typedCharacters: "Caracteres digitados",
-    add: "Agregar",
-    textValidation: "Debe digitar el texto de la tarea",
-  },
-  en: {
-    title: "Task Manager",
-    count: "Count",
-    newTask: "New task",
-    typedCharacters: "Typed characters",
-    add: "Add",
-    textValidation: "You must type the text of the task",
-  },
-  fr: {
-    title: "Gestionnaire des tâches",
-    count: "Montant",
-    newTask: "Nouveaux devoirs",
-    typedCharacters: "Caractères tapés",
-    add: "Ajouter",
-    textValidation: "Vous devez taper le texte de la tâche",
-  },
-};
 
 function App() {
   const [tasks, setTasks] = useState([]);
