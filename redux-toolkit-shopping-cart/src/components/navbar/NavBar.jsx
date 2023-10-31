cc6c088eb8736cb1b4b9fe0b70984518e26dc343
John Cardozo <johncardozo@gmail.com>
2023-10-31 08:21:01 -0500

    Redux: Estilos del component NavBar y acceso al Store através de useSelector

--- redux-toolkit-shopping-cart/src/components/navbar/NavBar.jsx
+++ redux-toolkit-shopping-cart/src/components/navbar/NavBar.jsx
@@ -1,12 +1,16 @@
 import { GrCart } from "react-icons/gr";
+import { useSelector } from "react-redux";
 
 const NavBar = () => {
+  // Obtiene un valor del state
+  const { amount } = useSelector((store) => store.cart);
+
   return (
     <header>
       <h1>My Store</h1>
       <div className="amount-container">
         <GrCart className="cart-icon" />
-        <span className="badge">4</span>
+        <span className="badge">{amount}</span>
       </div>
     </header>
   );
