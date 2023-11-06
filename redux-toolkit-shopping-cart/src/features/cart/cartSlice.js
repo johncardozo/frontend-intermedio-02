8892171713edf7d43335e558640d7b475b774fd6
John Cardozo <johncardozo@gmail.com>
2023-11-06 08:15:20 -0500

    Redux: Se crea el objeto inicializador del state en un archivo separado

--- redux-toolkit-shopping-cart/src/features/cart/cartSlice.js
+++ redux-toolkit-shopping-cart/src/features/cart/cartSlice.js
@@ -1,29 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../data/cartData";
 
 const initialState = {
-  cartItems: [
-    {
-      id: "1",
-      title: "Apple iPhone 15 Pro Max",
-      price: 10,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-15-pro-max.jpg",
-      amount: 1,
-    },
-    {
-      id: "2",
-      title: "Google Pixel 8 Pro",
-      price: 20,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/google-pixel-8-pro.jpg",
-      amount: 1,
-    },
-    {
-      id: "3",
-      title: "Samsung Galaxy S23 Ultra",
-      price: 30,
-      img: "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s23-ultra-5g.jpg",
-      amount: 1,
-    },
-  ],
+  cartItems,
   amount: 3,
   total: 60,
 };
@@ -34,7 +13,7 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     clearCart: (state) => {
-      // Elimina tooos los elementos del cart
+      // Elimina todos los elementos del cart
       state.cartItems = [];
     },
   },
