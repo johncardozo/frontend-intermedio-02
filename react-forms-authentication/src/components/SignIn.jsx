27ac92e2de43a11ed3b1dec9e9d444df1d369502
John Cardozo <johncardozo@gmail.com>
2023-11-14 06:36:41 -0500

    React Forms: useState para almacenar datos de un formulario

--- react-forms-authentication/src/components/SignIn.jsx
+++ react-forms-authentication/src/components/SignIn.jsx
@@ -1,23 +1,44 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
-import InputPassword from "./InputPassword";
 
 const SignIn = () => {
   let { setStep } = useContext(AuthContext);
+
+  // Estado del formulario
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlerSubmit = (e) => {
+    // Evita el envío de datos a un backend
+    e.preventDefault();
+    console.log("Sending data to backend...");
+    console.log(`El usuario digitó: ${email} y ${password}`);
+  };
+
   return (
     <main>
       <h3>Hello, friend!</h3>
       <div className="card">
-        <form autoComplete="off">
+        <form onSubmit={handlerSubmit} autoComplete="off">
           <fieldset>
             <label htmlFor="email">email</label>
-            <input type="email" id="email" autoFocus />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              autoFocus
+            />
             <p className="error">Valid email required</p>
           </fieldset>
           <fieldset>
-            <label htmlFor="password">password</label>
-            <InputPassword />
-            {/* <input type="password" id="password" /> */}
+            <label htmlFor="password">password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+            />
             <p className="error">Password is required</p>
           </fieldset>
           <button type="submit">login</button>
