c8c3dcaf4e3a085e9f13054d63a6d52e0efd5798
John Cardozo <johncardozo@gmail.com>
2023-11-20 06:59:09 -0500

    Angular: One way y Two way binding

--- angular17-expenses-tracker/src/app/components/balance/balance.component.ts
+++ angular17-expenses-tracker/src/app/components/balance/balance.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-balance',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './balance.component.html',
   styleUrl: './balance.component.scss',
 })
-export class BalanceComponent {}
+export class BalanceComponent {
+  // Variable con tipo implícito
+  title = 'This is the Balance Component';
+  // Variable con tipo explícito
+  search: string = 'iPhone 15 Pro Max';
+}
