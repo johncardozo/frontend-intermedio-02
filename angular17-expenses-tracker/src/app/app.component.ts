337e36a0b0331a8899d806e10f7a7d263c27fc03
John Cardozo <johncardozo@gmail.com>
2023-11-24 07:38:37 -0500

    Angular: Refactor utilizando los componentes Home, AddTransaction y About

--- angular17-expenses-tracker/src/app/app.component.ts
+++ angular17-expenses-tracker/src/app/app.component.ts
@@ -2,68 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
-// Components
-import { BalanceComponent } from './components/balance/balance.component';
-import { TransactionsComponent } from './components/transactions/transactions.component';
-
-// Models
-import { Balance } from './models/balance.model';
-import { Transaction } from './models/transaction.model';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    RouterLink,
-    BalanceComponent,
-    TransactionsComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  balance: Balance = {
-    amount: 55_000,
-    income: 100_000,
-    expenses: 45_000,
-  };
-  transactions: Transaction[] = [
-    {
-      id: '1',
-      type: 'expense',
-      amount: 45,
-      category: 'food',
-      date: new Date(2023, 11, 1),
-    },
-    {
-      id: '2',
-      type: 'expense',
-      amount: 280,
-      category: 'shopping',
-      date: new Date(2023, 11, 2),
-    },
-    {
-      id: '3',
-      type: 'expense',
-      amount: 60,
-      category: 'entertainment',
-      date: new Date(2023, 11, 3),
-    },
-    {
-      id: '4',
-      type: 'income',
-      amount: 500,
-      category: 'payroll',
-      date: new Date(2023, 11, 4),
-    },
-  ];
-
-  removeTransaction(id: string) {
-    // Elimina la transacción del arreglo
-    this.transactions = this.transactions.filter(
-      (transaction) => transaction.id !== id
-    );
-  }
-}
+export class AppComponent {}
--- angular17-expenses-tracker/src/app/pages/home/home.component.ts
+++ angular17-expenses-tracker/src/app/pages/home/home.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Components
+import { BalanceComponent } from '../../components/balance/balance.component';
+import { TransactionsComponent } from '../../components/transactions/transactions.component';
+
+// Models
+import { Balance } from '../../models/balance.model';
+import { Transaction } from '../../models/transaction.model';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BalanceComponent, TransactionsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  balance: Balance = {
+    amount: 55_000,
+    income: 100_000,
+    expenses: 45_000,
+  };
+  transactions: Transaction[] = [
+    {
+      id: '1',
+      type: 'expense',
+      amount: 45,
+      category: 'food',
+      date: new Date(2023, 11, 1),
+    },
+    {
+      id: '2',
+      type: 'expense',
+      amount: 280,
+      category: 'shopping',
+      date: new Date(2023, 11, 2),
+    },
+    {
+      id: '3',
+      type: 'expense',
+      amount: 60,
+      category: 'entertainment',
+      date: new Date(2023, 11, 3),
+    },
+    {
+      id: '4',
+      type: 'income',
+      amount: 500,
+      category: 'payroll',
+      date: new Date(2023, 11, 4),
+    },
+  ];
+  removeTransaction(id: string) {
+    // Elimina la transacción del arreglo
+    this.transactions = this.transactions.filter(
+      (transaction) => transaction.id !== id
+    );
+  }
 }
