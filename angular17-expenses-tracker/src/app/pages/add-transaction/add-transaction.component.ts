613eb3d1979b03547b1f3dc22ba46c3f7402d408
John Cardozo <johncardozo@gmail.com>
2023-11-27 08:02:23 -0500

    Angular: FormGroup y FormControl para manejo de ReactiveForms y evento ngSubmit

--- angular17-expenses-tracker/src/app/pages/add-transaction/add-transaction.component.ts
+++ angular17-expenses-tracker/src/app/pages/add-transaction/add-transaction.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+// Formularios
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-transaction',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './add-transaction.component.html',
   styleUrl: './add-transaction.component.scss',
 })
-export class AddTransactionComponent {}
+export class AddTransactionComponent implements OnInit {
+  // Objeto que administra la información del formulario
+  addTransactionForm!: FormGroup;
+
+  // Se ejecuta en el montaje del componente en el DOM Tree
+  ngOnInit(): void {
+    // Inicialización del formulario
+    this.addTransactionForm = new FormGroup({
+      amount: new FormControl(0),
+      type: new FormControl('expense'),
+      category: new FormControl(''),
+      date: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.addTransactionForm.value);
+  }
+}
