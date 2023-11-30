4c9e040b63d5a39fd1dacd44bbdb52ec79b74b48
John Cardozo <johncardozo@gmail.com>
2023-11-30 07:21:35 -0500

    Angular: Recalcula los saldos del componente Balance

--- angular17-expenses-tracker/src/app/pages/home/home.component.ts
+++ angular17-expenses-tracker/src/app/pages/home/home.component.ts
@@ -22,9 +22,9 @@ import { TransactionsService } from '../../services/transactions.service';
 export class HomeComponent implements OnInit {
   transactions!: Transaction[];
   balance: Balance = {
-    amount: 55_000,
-    income: 100_000,
-    expenses: 45_000,
+    amount: 0,
+    income: 0,
+    expenses: 0,
   };
 
   // Provee el servicio al componente usando Dependency Injection
@@ -33,6 +33,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.transactionService.get().subscribe((response: Transaction[]) => {
       this.transactions = response;
+      // Recalcula los saldos
+      this.calculateBalance();
     });
   }
 
@@ -44,6 +46,25 @@ export class HomeComponent implements OnInit {
       this.transactions = this.transactions.filter(
         (transaction) => transaction.id !== id
       );
+      // Recalcula los saldos
+      this.calculateBalance();
     });
+  }
+
+  /**
+   * Recalcula los saldos para el componente Balance
+   */
+  calculateBalance(): void {
+    let income: number = 0;
+    let expenses: number = 0;
+    // Recorre las transacciones para calcular los saldos
+    this.transactions.forEach((transaction) => {
+      if (transaction.type === 'expense') expenses += transaction.amount;
+      if (transaction.type === 'income') income += transaction.amount;
+    });
+    // Calcula el saldo total
+    const amount: number = income - expenses;
+    // Actualiza los saldos
+    this.balance = { amount, income, expenses };
   }
 }
